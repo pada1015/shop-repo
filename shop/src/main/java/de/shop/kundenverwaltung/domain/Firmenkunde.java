37a4b6075f090aa45c2cb34785c6a32a7fdfc49f
Sinan Mumcu <musi1012 (at) hs-karlsruhe.de>
2013-11-04 12:46:34 +0100

    Attribute, Methoden Firmenkunde erweitert

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Firmenkunde.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Firmenkunde.java
@@ -5,5 +5,14 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Firmenkunde extends AbstractKunde {
 	private static final long serialVersionUID = 6258156986876418100L;
-
+	private String firmenname;
+	
+	public String getFirmenname() {
+		return firmenname;
+	}
+	public void setFirmenname(String firmenname) {
+		this.firmenname = firmenname;
+	}
+	
+	//TODO geerbte ToString erweitern.
 }
