f6df2206871afab0b504ce96213f107154920a27
Rosen Vasilev <varo1012 (at) hs-karlsruhe.de>
2013-12-04 08:09:35 +0100

    Service Klassen

---
+++ shop/src/main/java/de/shop/bestellverwaltung/service/BestellungService.java
@@ -0,0 +1,13 @@
+package de.shop.bestellverwaltung.service;
+
+import java.util.List;
+import java.util.Locale;
+
+import de.shop.bestellverwaltung.domain.Bestellung;
+import de.shop.kundenverwaltung.domain.AbstractKunde;
+
+public interface BestellungService {
+	Bestellung findBestellungById(Long id);
+	List<Bestellung> findBestellungenByKunde(AbstractKunde kunde);
+	Bestellung createBestellung(Bestellung bestellung, AbstractKunde kunde, Locale locale);
+}
